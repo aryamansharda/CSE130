93def33bbc92d1cc4c94429aad6949699a1a64bb
Aryaman Sharda <asharda@ucsd.edu>
2016-12-10 15:12:21 -0700

    Initial commit.

--- CSE130.sh
+++ CSE130.sh
@@ -5,41 +5,41 @@ printf "Welcome to CSE 130! \nWould you like to (r)etrieve code files or (s)ubmi
 read response 
 
 if [ "$response" == "r" ]; then
-	printf "Creating CSE 130 folder..."
-	printf "Initializing git repository..."
+	printf "Creating CSE 130 folder...\n"
+	printf "Initializing git repository...\n"
 	mkdir "CSE 130"
 	git init
 	git add *
 	git commit -m "Initial commit."
 
-	printf "Creating PA1 folder..."
+	printf "Creating PA1 folder...\n"
 	mkdir "./CSE 130/PA1"
-	printf "Downloading PA1 starter code..."
+	printf "Downloading PA1 starter code...\n"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa1/misc.ml" --silent --output "./CSE 130/PA1/misc.ml"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa1/test.ml" --silent --output "./CSE 130/PA1/test.ml"
 
-	printf "Creating PA2 folder..."
+	printf "Creating PA2 folder...\n"
 	mkdir "./CSE 130/PA2"
-	printf "Downloading PA2 starter code..."
+	printf "Downloading PA2 starter code...\n"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa2/misc.ml" --silent --output "./CSE 130/PA2/misc.ml"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa2/test.ml" --silent --output "./CSE 130/PA2/test.ml"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa2/art.ml" --silent --output "./CSE 130/PA2/test.ml"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa2/expr.ml" --silent --output "./CSE 130/PA2/test.ml"
 
-	printf "Creating PA3 folder..."
+	printf "Creating PA3 folder...\n"
 	mkdir "./CSE 130/PA3"
-	printf "Downloading PA3 starter code..."
+	printf "Downloading PA3 starter code...\n"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa3/misc.ml" --silent --output "./CSE 130/PA3/misc.ml"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa3/test.ml" --silent --output "./CSE 130/PA3/test.ml"
 
-	printf "Creating PA4 folder..."
+	printf "Creating PA4 folder...\n"
 	mkdir "./CSE 130/PA4"
-	printf "Downloading PA4 starter code..."
+	printf "Downloading PA4 starter code...\n"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa4/pa4.zip" --silent --output "./CSE 130/PA4/pa4.zip"
 
-	printf "Creating PA5 folder..."
+	printf "Creating PA5 folder...\n"
 	mkdir "./CSE 130/PA5"
-	printf "Downloading PA5 starter code..."
+	printf "Downloading PA5 starter code...\n"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa5/misc.py" --silent --output "./CSE 130/PA5/misc.py"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa5/crack.py" --silent --output "./CSE 130/PA5/crack.py"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa5/test.py" --silent --output "./CSE 130/PA5/test.py"
@@ -47,18 +47,18 @@ if [ "$response" == "r" ]; then
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa5/passwd" --silent --output "./CSE 130/PA5/passwd"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa5/news.txt" --silent --output "./CSE 130/PA5/news.txt"
 
-	printf "Creating PA6 folder..."
+	printf "Creating PA6 folder...\n"
 	mkdir "./CSE 130/PA6"
-	printf "Downloading PA6 starter code..."
+	printf "Downloading PA6 starter code...\n"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa6/misc.py" --silent --output "./CSE 130/PA6/misc.py"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa6/vector.py" --silent --output "./CSE 130/PA6/vector.py"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa6/decorators.py" --silent --output "./CSE 130/PA6/decorators.py"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa6/test.py" --silent --output "./CSE 130/PA6/test.py"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa6/decorators.out" --silent --output "./CSE 130/PA6/decorators.out"
 
-	printf "Creating PA7 folder..."
+	printf "Creating PA7 folder...\n"
 	mkdir "./CSE 130/PA7"
-	printf "Downloading PA7 starter code..."
+	printf "Downloading PA7 starter code...\n"
 	curl "https://cseweb.ucsd.edu/classes/fa16/cse130-a/pa7/misc.pl" --silent --output "./CSE 130/PA7/misc.pl"	
 
 	printf "All starter code has been retrieved"
